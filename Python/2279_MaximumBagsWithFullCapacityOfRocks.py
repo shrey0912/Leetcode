9262f4d3457de8326c23a06dd07655bec8f46280
Shreyas Jain <88982617+shrey0912@users.noreply.github.com>
2022-05-22 12:41:43 +0530

    Update 2279_MaximumBagsWithFullCapacityOfRocks.py

--- Python/2279_MaximumBagsWithFullCapacityOfRocks.py
+++ Python/2279_MaximumBagsWithFullCapacityOfRocks.py
@@ -1,1 +1,12 @@
-
+class Solution:
+    def maximumBags(self, capacity: List[int], rocks: List[int], additionalRocks: int) -> int:
+        remcap=[capacity[i]-rocks[i] for i in range(len(rocks))]
+        remcap.sort()
+        fill=0
+        tot=0
+        for r in remcap:
+            if fill+r<=additionalRocks:
+                fill+=r
+                tot+=1
+        return tot
+        
